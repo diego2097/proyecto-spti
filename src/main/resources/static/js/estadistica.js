1f73495405317fa966bc250f69c60a4e1bcc3b34
dieg2097 <2125509@labinfo.is.escuelaing.edu.co>
2019-11-13 11:26:23 -0500

    archivo funciona

--- src/main/resources/static/js/estadistica.js
+++ src/main/resources/static/js/estadistica.js
@@ -1,13 +1,15 @@
 var estadisticaModule = (function() {
   var _contrasenas = null;
   var dic = {};
-  
-   
+  var debiles = 0;  
+  var medias = 0; 
+  var fuertes = 0;
 
   _getContrasena = function(contrasenas) {
     _contrasenas = contrasenas;
     console.log(_contrasenas);
-    for (i = 0; i < _contrasenas.length; i++) {
+     for (i = 0; i < _contrasenas.length; i++) {
+      _evaluatePassword(_contrasenas[i]);      
       if (_contrasenas[i] in dic) {
         dic[_contrasenas[i]]++;
       } else {
@@ -15,6 +17,44 @@ var estadisticaModule = (function() {
       }
     }
   };
+
+
+  _evaluatePassword = function(password){
+    console.log(password);
+    const regxs = {
+      "lower": /^[a-z]+$/,
+      "upper": /^[A-Z]+$/,
+      "upperLower": /^[A-Za-z]+$/,
+      "number": /^[0-9]+$/,
+      "punt": /^[!"#$%&'()*+,\-./:;<=>?@[]_`{}]+$/,
+      "puntNumber": /^[0-9!"#$%&'()*+,\-./:;<=>?@[]_`{}]+$/
+    };
+      if (regxs.puntNumber.test(password)){
+        console.log(3);
+      }
+      else if (regxs.punt.test(password) && regxs.upper.test(password)){
+        console.log(3);
+      }
+      else if (regxs.punt.test(password) && regxs.lower.test(password)){
+        console.log(3);
+      }
+      else if (regxs.lower.test(password)) {
+        console.log(1);
+      }
+      else if (regxs.upper.test(password)){
+        console.log(11);
+      }
+      else if (regxs.number.test(password)){
+        console.log(111);
+      }
+      else if (regxs.punt.test(password)){
+        console.log(1111);
+      }
+      else if (regxs.upperLower.test(password)){
+        console.log(2);
+      }
+     
+  }
 
   function leerArchivo(e) {
     var archivo = e.target.files[0];
